fcfedb7d41734092df95127c9077a29905a3e2e1
Carlos López Gallardo <124450022+cegallardo0405@users.noreply.github.com>
2024-06-28 18:47:03 -0500

    chore: Update deploy branch to carlos-dev and site URL in astro.config.mjs

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://cegallardo0405.github.io",
-  base: "/",
+  site: "https://suscantillo.github.io",
+  base: "/krea-front-repo",
 });
